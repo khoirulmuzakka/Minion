b5f1c25f9de73d4c15b10f2ded1ee2020b3b987a
Muzakka <khoirul.muzakka@gmail.com>
2025-01-27 00:32:23 +0100

    Added setup.py for scikit-build

---
+++ setup.py
@@ -0,0 +1,11 @@
+from skbuild import setup
+
+setup(
+    name="minionpy",
+    version="0.1.2",
+    description="MinionPy is the Python implementation of the Minion C++ library, designed for derivative-free optimization.",
+    author="Khoirul Faiq Muzakka",
+    license="MIT",
+    packages=["minionpy"],
+    python_requires=">=3.7",
+)
