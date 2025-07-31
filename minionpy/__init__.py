8e94353428dcad2b3d06f47492d577d27bb5ef9c
Muzakka <khoirul.muzakka@gmail.com>
2025-07-31 12:05:58 +0200

    Updated init

--- minionpy/__init__.py
+++ minionpy/__init__.py
@@ -7,7 +7,7 @@ try : from .cec_2011 import CEC2011
 except : pass
 
 # Define the package's version
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 
 # Optionally, define the package name
 __name__ = "minionpy"
